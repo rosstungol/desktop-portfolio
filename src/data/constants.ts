bae3a8abbe67de14d663a87acd0ebededc7c5b75
Ross Tungol <rossjstungol@gmail.com>
2026-02-20 05:57:41 +0800

    adds desktop dock

---
+++ src/data/constants.ts
@@ -0,0 +1,27 @@
+export const dockApps = [
+	{
+		id: 'finder',
+		name: 'Finder',
+		icon: 'finder.png',
+	},
+	{
+		id: 'safari',
+		name: 'Safari',
+		icon: 'safari.png',
+	},
+	{
+		id: 'contact',
+		name: 'Contact',
+		icon: 'contact.png',
+	},
+	{
+		id: 'photos',
+		name: 'Photos',
+		icon: 'photos.png',
+	},
+	{
+		id: 'terminal',
+		name: 'Terminal',
+		icon: 'terminal.png',
+	},
+]
--- src/features/desktop/components/DesktopScreen.tsx
+++ src/features/desktop/components/DesktopScreen.tsx
@@ -1,9 +1,11 @@
+import Dock from './Dock'
 import Navbar from './Navbar'
 
 export default function DesktopScreen() {
 	return (
 		<div>
 			<Navbar />
+			<Dock />
 		</div>
 	)
 }
