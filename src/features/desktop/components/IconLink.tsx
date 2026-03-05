851dfa318329f3cd135341a1000cce3f5ddecb76
Ross Tungol <rossjstungol@gmail.com>
2026-03-05 23:18:00 +0800

    adds icon link component

---
+++ src/features/desktop/components/IconLink.tsx
@@ -0,0 +1,46 @@
+import clsx from 'clsx'
+import type { AnchorHTMLAttributes } from 'react'
+
+type IconLinkSize = 'md' | 'sm'
+
+type IconLinkProps = AnchorHTMLAttributes<HTMLAnchorElement> & {
+	label: string
+	size?: IconLinkSize
+	rounded: boolean
+}
+
+const baseStyles =
+	'flex-center border-2 border-blue-500/10 font-bold text-gray-200'
+const hoverStyles =
+	'transition-colors hover:border-blue-100 hover:bg-blue-100 hover:text-gray-950'
+
+const sizeStyles: Record<IconLinkSize, string> = {
+	sm: 'p-1',
+	md: 'p-2',
+}
+
+export default function IconLink({
+	href,
+	label,
+	size = 'md',
+	rounded,
+	children,
+	...props
+}: IconLinkProps) {
+	return (
+		<a
+			href={href}
+			aria-label={label}
+			title={label}
+			className={clsx(
+				baseStyles,
+				hoverStyles,
+				sizeStyles[size],
+				rounded ? 'rounded-full' : 'rounded-lg'
+			)}
+			{...props}
+		>
+			{children}
+		</a>
+	)
+}
