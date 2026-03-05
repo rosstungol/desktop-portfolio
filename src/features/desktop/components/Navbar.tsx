5b830d8ceb0b727185c6f2a8f92bc9e8f9215cae
Ross Tungol <rossjstungol@gmail.com>
2026-03-05 22:58:21 +0800

    updates navbar component styles

--- src/features/desktop/components/Navbar.tsx
+++ src/features/desktop/components/Navbar.tsx
@@ -4,7 +4,7 @@ import DigitalClock from './DigitalClock'
 
 export default function Navbar() {
 	return (
-		<nav className='glass flex w-full items-center justify-between px-4 py-0.5 text-gray-200 text-xs'>
+		<nav className='glass flex w-full items-center justify-between border-blue-500/10 border-b px-4 py-0.5 text-gray-200 text-xs'>
 			<div className='flex items-center gap-2 font-bold'>
 				<Banana size={10} />
 				<p>Ross Tungol</p>
