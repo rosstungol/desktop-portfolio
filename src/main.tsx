c7a1a82d85d050082198693ded8db14c4a6b8bf2
Ross Tungol <rossjstungol@gmail.com>
2026-03-10 00:19:16 +0800

    adds gsap registration

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import './assets/index.css'
 
 import { App } from './App'
+
+gsap.registerPlugin(useGSAP)
 
 const root = document.getElementById('root')
 
