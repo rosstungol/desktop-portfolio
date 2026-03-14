224cbe08bacf533c269f293b619c4a329235011d
Ross Tungol <rossjstungol@gmail.com>
2026-03-15 03:08:12 +0800

    adds image fallback

--- src/features/desktop/components/windows/ImageViewer.tsx
+++ src/features/desktop/components/windows/ImageViewer.tsx
@@ -1,27 +1,40 @@
+import { type ReactNode, useState } from 'react'
+
 import { useWindowStore } from '../../stores/window'
 import { WindowHeader } from '../wrapper/WindowHeader'
 import { WindowWrapper } from '../wrapper/WindowWrapper'
 
 function ImageViewer() {
-	const windows = useWindowStore((state) => state.windows)
-	const data = windows.imageFile?.data
+	const [hasError, setHasError] = useState(false)
+	const data = useWindowStore((state) => state.windows.imageFile?.data)
 
 	if (!data) return null
 
 	const { name, fileContent } = data
+
+	const imageFallback: ReactNode = (
+		<div className='p-4 text-center text-red-400'>Failed to load image</div>
+	)
 
 	return (
 		<>
 			<WindowHeader title={name} target={'imageFile'} />
 			<div className='p-2'>
 				{fileContent?.image ? (
-					<div className='max-h-[500px] w-full flex-center overflow-clip rounded-md'>
-						<img src={fileContent.image} alt={name} className='w-full' />
+					<div className='max-h-[500px] flex-center overflow-hidden rounded-lg'>
+						{hasError ? (
+							imageFallback
+						) : (
+							<img
+								src={fileContent.image}
+								alt={name}
+								className='block h-auto max-w-full object-cover'
+								onError={() => setHasError(true)}
+							/>
+						)}
 					</div>
 				) : (
-					<div className='p-4 text-center text-red-400'>
-						Failed to load image
-					</div>
+					imageFallback
 				)}
 			</div>
 		</>
