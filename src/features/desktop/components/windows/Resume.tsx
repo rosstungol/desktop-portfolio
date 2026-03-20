14c2659ba307434e8293faa24eadbfccecfdfe83
Ross Tungol <rossjstungol@gmail.com>
2026-03-20 23:49:50 +0800

    updates resume pdf worker

--- src/features/desktop/components/windows/Resume.tsx
+++ src/features/desktop/components/windows/Resume.tsx
@@ -9,10 +9,7 @@ import { IconLink } from '../ui/IconLink'
 import { WindowHeader } from '../wrapper/WindowHeader'
 import { WindowWrapper } from '../wrapper/WindowWrapper'
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-	'pdfjs-dist/build/pdf.worker.min.mjs',
-	import.meta.url
-).toString()
+pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`
 
 function Resume() {
 	return (
