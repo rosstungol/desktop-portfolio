d75d6c90fe6ca0dcb6a7bc2ea9aac91a5e381ae9
Ross Tungol <rossjstungol@gmail.com>
2026-05-11 14:30:03 +0800

    updates text viewer styles

--- src/features/desktop/components/windows/TextViewer.tsx
+++ src/features/desktop/components/windows/TextViewer.tsx
@@ -12,7 +12,7 @@ function TextViewer() {
 	return (
 		<>
 			<WindowHeader title={name} target='file' />
-			<div className='flex max-h-[880px] min-h-[200px] flex-col gap-2 overflow-y-auto rounded-b-2xl bg-gray-900/60 px-10 py-16 text-3xl'>
+			<div className='flex max-h-[880px] min-h-[200px] flex-col gap-2 overflow-y-auto rounded-b-2xl bg-gray-900/60 px-10 py-16'>
 				{fileContent?.image && (
 					<div className='mb-6 max-h-60 w-full flex-center overflow-clip rounded'>
 						<img src={fileContent.image} alt={name} loading='lazy' />
@@ -20,11 +20,13 @@ function TextViewer() {
 				)}
 
 				{fileContent?.title && (
-					<h2 className='mb-6 font-bold'>{fileContent.title}</h2>
+					<h2 className='mb-6 font-bold text-3xl'>{fileContent.title}</h2>
 				)}
 
 				{fileContent?.text && (
-					<div className='leading-relaxed'>{fileContent.text}</div>
+					<div className='space-y-8 text-content leading-relaxed'>
+						{fileContent.text}
+					</div>
 				)}
 			</div>
 		</>
