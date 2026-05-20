b486401a750d7ab4277723296298aded89632a16
Ross Tungol <rossjstungol@gmail.com>
2026-05-20 20:44:18 +0800

    udpates access key

--- src/features/desktop/components/forms/ContactForm.tsx
+++ src/features/desktop/components/forms/ContactForm.tsx
@@ -77,7 +77,7 @@ export function ContactForm() {
 
 		const payload = {
 			...formData,
-			access_key: 'de66a0f7-147b-4815-86b8-5e58c84b15f5',
+			access_key: 'a5256817-013d-4d37-b8f6-9da323270ab8',
 		}
 
 		if (!payload.access_key) {
