132759fd1c296409ac964f2a5a5ee0f2ba9371b0
Ross Tungol <rossjstungol@gmail.com>
2026-06-08 23:02:34 +0800

    adds new tab link to iframe

--- src/features/desktop/components/windows/LowOrbit.tsx
+++ src/features/desktop/components/windows/LowOrbit.tsx
@@ -1,4 +1,7 @@
+import { SquareArrowOutUpRight } from 'lucide-react'
+
 import { playground } from '@/data/constants/projects'
+
 import { useWindowStore } from '../../stores/window'
 import { WindowHeader } from '../wrapper/WindowHeader'
 import { WindowWrapper } from '../wrapper/WindowWrapper'
@@ -12,13 +15,22 @@ function LowOrbit() {
 		<>
 			<WindowHeader title='Low Orbit' target='lowOrbit' />
 			{lowOrbitIsOpen && (
-				<div className='h-225 w-450'>
+				<div className='relative h-225 w-450'>
 					<iframe
 						title='Low Orbit'
 						src={playground.lowOrbit.url}
 						className='h-full w-full'
 						sandbox='allow-scripts'
 					/>
+					<a
+						href={playground.lowOrbit.url}
+						target='_blank'
+						rel='noopener noreferrer'
+						className='absolute right-4 bottom-4 flex items-center gap-2 font-bold font-mono text-xl drop-shadow drop-shadow-neutral-900/50'
+					>
+						Open in new tab
+						<SquareArrowOutUpRight size={18} />
+					</a>
 				</div>
 			)}
 		</>
